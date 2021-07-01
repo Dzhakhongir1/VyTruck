d83cc4fc944da2f5ddacc22e4f03141d5b20e9cb
Jahongir <islamovjaha7@gmail.com>
2021-06-30 21:48:57 -0500

    Update more Hello feature

--- src/test/java/TEst.java
+++ src/test/java/TEst.java
@@ -2,6 +2,8 @@ public class TEst {
     public static void main(String[] args) {
         System.out.println("hello Test");
         System.out.println("Developing hello feature");
+        System.out.println("local repo is already published to git hub");
+        System.out.println("Doing more work");
     }
 
 }
